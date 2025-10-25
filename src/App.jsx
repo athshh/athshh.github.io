0413c46bf4e00201233f7829536d205c81acd3b8
Athiban Sharon <athibansharon2008@gmail.com>
2025-10-25 09:41:46 +0300

    feat: changing style entirely

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,42 @@ import styles from './App.module.css'
 import About from './About.jsx'
 
 function App() {
+	const move = document.getElementById("move");
+
+	document.body.onpointermove = event => {
+		const { clientX, clientY } = event;
+
+		move.animate({
+			left: `${clientX}px`,
+			top: `${clientY}px`
+
+		}, { duration: 1000, fill: "forwards" })
+
+	}
 	return (
 		<>
 			<Parallax pages={5}>
-				<ParallaxLayer offset={0} factor={1.5} speed={2.0} style={{backgroundColor: `black`, backgroundImage: `url(${Stars})`}}>
-					<Landing />
+				<ParallaxLayer offset={0} factor={1.5} speed={2.0}>
+					<div className={styles.landingBG} id="move">
+						<style></style>
+						<div className={styles.circle} style={{
+							aspectRatio: '1',
+							left: '50%',
+							right: '50%',
+							transform: 'translate(-50%, -50%)',
+							height: '0',
+							width: '0',
+							backgroundColor: 'rgba(0, 0, 0, 0.7)',
+							border: 'none',
+							borderRadius: '100%',
+							boxShadow: '0 0 128px 100px rgba(0, 0, 0, 0.7)'
+						}}></div>
+					</div>
+					<div className={styles.landingFG}>
+						<Landing />
+					</div>
 				</ParallaxLayer>
-				<ParallaxLayer offset={0.5} factor={2} speed={1.5} style={{backgroundImage: `url(${Skyline})`}}>
+				<ParallaxLayer offset={0.5} factor={2} speed={1.5} style={{ backgroundImage: `url(${Skyline})` }}>
 					<About />
 				</ParallaxLayer>
 
