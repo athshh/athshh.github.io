4652418dfbd38353ed5ba90a1e3228ecae603d65
Athiban Sharon <athibansharon2008@gmail.com>
2025-11-24 20:38:37 +0300

    fix: onclick test

--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -1,29 +1,33 @@
 import styles from './Skills.module.css'
 import Card from './Card.jsx'
 
+
 function Skills() {
-	return(
-	<>
+	function test() {
+		alert("hello world");
+	}
+	return (
+		<>
 			<div className={styles.mainContent}>
-				<h1 style={{ marginBottom: "48px",color:"black"}}>Skills.</h1>
+				<h1 style={{ marginBottom: "48px", color: "black" }}>Skills.</h1>
 				<div className={styles.container}>
-					<div className={styles.containerItem} style={{ marginTop: "2rem", textAlign:"justify",textJustify:"auto" }} >
+					<div className={styles.containerItem} style={{ marginTop: "2rem", textAlign: "justify", textJustify: "auto" }} >
 						<p>Over the years, I have collected a diverse range of skills.From regular programming, to AI, the list goes on and will keep growing. I've listed them here.</p>
 					</div>
 					<div className={styles.containerItem}>
 						<div className={styles.btnGroup}>
-							<button>Programming</button>
-							<button>Programming</button>
-							<button>Programming</button>
-							<button>Programming</button>
+							<a onClick={test} className={styles.btn}>Programming</a>
+							<a className={styles.btn}>Programming</a>
+							<a className={styles.btn}>Programming</a>
+							<a className={styles.btn}>Programming</a>
 						</div>
-					</div>	
+					</div>
 				</div>
 
 			</div>
-	</>
+		</>
 	)
 }
 
-export default Skills 
+export default Skills
 
