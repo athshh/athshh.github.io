8060fa9c4b085ceaa64da20458c506d85eedd933
Athiban Sharon <athibansharon2008@gmail.com>
2025-12-09 16:41:48 +0300

    feat: skills cards started

--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -9,11 +9,23 @@ const skills = [
 	"Databases"
 ]
 
+const skillTable = {
+	"Programming":"skillsProgramming",
+	"AI/ML":"skillsAI",
+	"DevOps":"skillsDevOps",
+	"Databases":"skillsDB"
+}
 
 function Skills() {
 	const [activeSkill, setActiveSkill] = useState(null);
-	function test() {
-		alert("hello world");
+	function changeView(skillName) {
+		for (var i = 0;i<4;i++) {
+			var currentSkill = document.getElementById(skillTable[skillName])
+			console.log(currentSkill)
+			var currentSkillID = skillTable[currentSkill]
+			console.log(currentSkillID)
+		}
+		console.log(oldActive);
 	}
 	return (
 		<>
@@ -26,8 +38,26 @@ function Skills() {
 					<div className={styles.containerItem}>
 						<div className={styles.btnGroup}>
 							{skills.map((skillName) => (
-							<button key={skillName} type="button" onclick={() => setActiveSkill(skillName)} className={[styles.CategoryButton, activeSkill === skillName ? styles.activeButton : ''].join(' ')}>{skillName}</button>
+							<button key={skillName} type="button" onClick={() => {setActiveSkill(skillName); changeView(skillName);}} className={[styles.CategoryButton, activeSkill === skillName ? styles.activeButton : ''].join(' ')} id={skillTable[skillName]}>{skillName}</button>
 							))}
+						</div>
+						<div className={styles.skills}>
+							<div className={styles.skillsProgramming} id="skillsProgrammingCards">
+								<Card title="hi" description="hello"></Card>
+								<Card title="hi" description="hello"></Card>
+							</div>
+							<div className={styles.skillsAI} id="skillsAICards">
+								<Card title="hi" description="2123e"></Card>
+								<Card title="hi" description="212o"></Card>
+							</div>
+							<div className={styles.skillsDevOps} id="skillsDevOpsCards">
+								<Card title="hi" description="1"></Card>
+								<Card title="hi" description="2"></Card>
+							</div>
+							<div className={styles.skillsDB} id="skillsDBCards">
+								<Card title="hi" description="2"></Card>
+								<Card title="hi" description="3"></Card>
+							</div>
 						</div>
 					</div>
 				</div>
